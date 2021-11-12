2d848b03ab3b6aeee5358993c1f175a657465d9f
alecdwm <alec@owls.io>
2021-11-12 13:24:02 +1000

    added handlers

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,33 +1,47 @@
-import {SubstrateExtrinsic,SubstrateEvent,SubstrateBlock} from "@subql/types";
-import {StarterEntity} from "../types";
-import {Balance} from "@polkadot/types/interfaces";
+import BigNumber from "bignumber.js";
+import { Contributor } from "../types";
+import { SubstrateEvent } from "@subql/types";
 
+const moonbeamParachainId = 2004;
 
-export async function handleBlock(block: SubstrateBlock): Promise<void> {
-    //Create a new starterEntity with ID using block hash
-    let record = new StarterEntity(block.block.header.hash.toString());
-    //Record block number
-    record.field1 = block.block.header.number.toNumber();
-    await record.save();
+export async function handleContributed(event: SubstrateEvent): Promise<void> {
+  // const eventId = `${event.block.block.header.number.toString()}-${event.idx}`;
+
+  // get event data
+  const [address, parachainId, contributionAmount] = event.event.data;
+
+  // ignore if this contribution isn't for moonbeam
+  if (!parachainId.eq(moonbeamParachainId)) return;
+
+  // get the contributor
+  const id = address.toString();
+  const contributor = (await Contributor.get(id)) || Contributor.create({ id });
+
+  // update their contribution total
+  contributor.totalContributed = new BigNumber(
+    contributor.totalContributed || "0"
+  )
+    .plus(new BigNumber(contributionAmount.toString()))
+    .toString();
+
+  // save
+  await contributor.save();
 }
 
-export async function handleEvent(event: SubstrateEvent): Promise<void> {
-    const {event: {data: [account, balance]}} = event;
-    //Retrieve the record by its ID
-    const record = await StarterEntity.get(event.extrinsic.block.block.header.hash.toString());
-    record.field2 = account.toString();
-    //Big integer type Balance of a transfer event
-    record.field3 = (balance as Balance).toBigInt();
-    await record.save();
+export async function handleMemoUpdated(event: SubstrateEvent): Promise<void> {
+  // get event data
+  const [address, parachainId, rewardsAddress] = event.event.data;
+
+  // ignore if this memo isn't for moonbeam
+  if (!parachainId.eq(moonbeamParachainId)) return;
+
+  // get the contributor
+  const id = address.toString();
+  const contributor = (await Contributor.get(id)) || Contributor.create({ id });
+
+  // set their rewardsAddress to the one specified in the memo
+  contributor.rewardsAddress = rewardsAddress.toHex();
+
+  // save
+  await contributor.save();
 }
-
-export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
-    const record = await StarterEntity.get(extrinsic.block.block.header.hash.toString());
-    //Date type timestamp
-    record.field4 = extrinsic.block.timestamp;
-    //Boolean tyep
-    record.field5 = true;
-    await record.save();
-}
-
-
